a54fb7728d313a006db94d2631f4a9034d4c33e6
artem55555p <temchan5@list.ru>
2015-06-10 12:59:39 +0300

    ps

---
+++ sum.pas
@@ -0,0 +1,15 @@
+program sum;
+var a,b,c:integer;
+begin
+writeln('Vvedite Slagaemiye!!!!');
+readln(a,b);
+c:=a+b;
+writeln(a,'+',b,'=',c);
+<<<<<<< HEAD
+//add
+=======
+
+>>>>>>> add coment
+//s
+//st
+end.
